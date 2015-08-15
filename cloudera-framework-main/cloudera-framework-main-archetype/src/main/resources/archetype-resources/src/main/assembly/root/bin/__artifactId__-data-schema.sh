da583071fb5b10ffb27def4d005a907167f5f996
ggear <graham.gear@gmail.com>
2015-08-15 18:19:25 +1000

    Update table create DDL scripts

--- cloudera-framework-main/cloudera-framework-main-archetype/src/main/resources/archetype-resources/src/main/assembly/root/bin/__artifactId__-data-schema.sh
+++ cloudera-framework-main/cloudera-framework-main-archetype/src/main/resources/archetype-resources/src/main/assembly/root/bin/__artifactId__-data-schema.sh
@@ -11,12 +11,12 @@ USER_ADMIN=${2:-"$USER_ADMIN"}
 NAME_SPACE_SERVER=${3:-"$NAME_SPACE_SERVER"}
 NAME_SPACE_DATABASE=${4:-"$NAME_SPACE_DATABASE"}
 ROOT_DIR_HDFS=${5:-"$ROOT_DIR_HDFS"}
-ROOT_DIR_HDFS_RAW_SOURCE=${6:-"ROOT_DIR_HDFS_RAW_SOURCE"}
-ROOT_DIR_HDFS_STAGED_PARTITIONED=${7:-"ROOT_DIR_HDFS_STAGED_PARTITIONED"}
-ROOT_DIR_HDFS_STAGED_MALFORMED=${8:-"ROOT_DIR_HDFS_STAGED_MALFORMED"}
-ROOT_DIR_HDFS_PROCESSED_CLEANSED=${9:-"ROOT_DIR_HDFS_PROCESSED_CLEANSED"}
-ROOT_DIR_HDFS_PROCESSED_DUPLICATE=${10:-"ROOT_DIR_HDFS_PROCESSED_DUPLICATE"}
-ROOT_DIR_HDFS_PROCESSED_MALFORMED=${11:-"ROOT_DIR_HDFS_PROCESSED_MALFORMED"}
+ROOT_DIR_HDFS_RAW_SOURCE=${6:-"$ROOT_DIR_HDFS_RAW_SOURCE"}
+ROOT_DIR_HDFS_STAGED_PARTITIONED=${7:-"$ROOT_DIR_HDFS_STAGED_PARTITIONED"}
+ROOT_DIR_HDFS_STAGED_MALFORMED=${8:-"$ROOT_DIR_HDFS_STAGED_MALFORMED"}
+ROOT_DIR_HDFS_PROCESSED_CLEANSED=${9:-"$ROOT_DIR_HDFS_PROCESSED_CLEANSED"}
+ROOT_DIR_HDFS_PROCESSED_DUPLICATE=${10:-"$ROOT_DIR_HDFS_PROCESSED_DUPLICATE"}
+ROOT_DIR_HDFS_PROCESSED_MALFORMED=${11:-"$ROOT_DIR_HDFS_PROCESSED_MALFORMED"}
 
 if [ $($ROOT_DIR/bin/*-shell-impala.sh -q "SHOW ROLES" 2> /dev/null| grep $USER_ADMIN|wc -l) -eq 0 ]; then
   $ROOT_DIR/bin/*-shell-hive.sh \
@@ -41,7 +41,7 @@ TABLES_DDL=( \
 #	"table_create_batch_name.ddl" \
 #	"table_create_batch_name.ddl" \
 	"table_create_batch_year_month.ddl" \
-	"table_create_batch_year_month.ddl" \
+#	"table_create_batch_year_month.ddl" \
 #	"table_create_batch_name.ddl" \
 #	"table_create_batch_name.ddl" \
 )
@@ -53,7 +53,7 @@ TABLES_NAME=( \
 #	"staged_malformed_text_xml_none" \
 #	"staged_malformed_text_csv_none" \
 	"processed_cleansed_avro_binary_none" \
-	"processed_duplicate_avro_binary_none" \
+#	"processed_duplicate_avro_binary_none" \
 #	"processed_malformed_xml_none" \
 #	"processed_malformed_csv_none" \
 )
@@ -65,7 +65,7 @@ TABLES_FORMAT=( \
 #	"com.cloudera.example.model.input.RecordTextInputFormatXml" \
 #	"com.cloudera.example.model.input.RecordTextInputFormatCsv" \
 	"org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat" \
-	"org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat" \
+#	"org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat" \
 #	"com.cloudera.example.model.input.RecordTextInputFormatXml" \
 #	"com.cloudera.example.model.input.RecordTextInputFormatCsv" \
 )
@@ -77,21 +77,21 @@ TABLES_LOCATION=( \
 #	"$ROOT_DIR_HDFS_STAGED_MALFORMED\text\xml\none" \
 #	"$ROOT_DIR_HDFS_STAGED_MALFORMED\text\csv\none" \
 	"$ROOT_DIR_HDFS_PROCESSED_CLEANSED\avro\binary\none" \
-	"$ROOT_DIR_HDFS_PROCESSED_DUPLICATE\avro\binary\none" \
+#	"$ROOT_DIR_HDFS_PROCESSED_DUPLICATE\avro\binary\none" \
 #	"$ROOT_DIR_HDFS_PROCESSED_MALFORMED\text\xml\none" \
 #	"$ROOT_DIR_HDFS_PROCESSED_MALFORMED\text\csv\none" \
 )
 
-AVRO_SCHEMA=$(cat $ROOT_DIR/lib/cfg/avro/model.avsc)
+AVRO_SCHEMA="$(cat $ROOT_DIR/lib/cfg/avro/model.avsc | tr '\n' ' ' | tr '\t' ' ')"
 
 for((i=0;i<${#TABLES_NAME[@]};i++)); do
   if ! $ROOT_DIR/bin/*-shell-hive.sh -e "MSCK REPAIR TABLE ${TABLES_NAME[$i]}; DESCRIBE ${TABLES_NAME[$i]}" 2> /dev/null; then
     $ROOT_DIR/bin/*-shell-hive.sh \
-      --hivevar my.table.name=${TABLES_NAME[$i]} \
-      --hivevar my.table.format=${TABLES_FORMAT[$i]} \
-      --hivevar my.table.location=${TABLES_LOCATION[$i]} \
-      --hivevar my.table.schema=$AVRO_SCHEMA \
-      -f $ROOT_DIR/lib/ddl/${TABLES_DDL[$i]}
+      --hivevar my.table.name="${TABLES_NAME[$i]}" \
+      --hivevar my.table.format="${TABLES_FORMAT[$i]}" \
+      --hivevar my.table.location="${TABLES_LOCATION[$i]}" \
+      --hivevar my.table.schema="$AVRO_SCHEMA" \
+      -f $ROOT_DIR/lib/ddl/hive/"${TABLES_DDL[$i]}"
     $ROOT_DIR/bin/*-shell-impala.sh -q "INVALIDATE METADATA ${TABLES_NAME[$i]};"
   else
     $ROOT_DIR/bin/*-shell-impala.sh -q "REFRESH ${TABLES_NAME[$i]};"
