59c123c97b318dc07853a9fee32707e49608c76f
ggear <graham.gear@gmail.com>
2016-06-19 11:03:41 +0800

    Update parcel environment

--- cloudera-framework-example/src/main/parcel/meta/cloudera-framework-example-env.sh
+++ cloudera-framework-example/src/main/parcel/meta/cloudera-framework-example-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
 $ROOT_DIR/cloudera-framework-initialise.sh
 
-export HIVE_AUX_JARS_PATH="$ROOT_DIR/../lib"
+export HIVE_AUX_JARS_PATH="$ROOT_DIR/lib/jar"
 export HADOOP_CLASSPATH="$HADOOP_CLASSPATH":"$(echo -n $(ls -m $ROOT_DIR/lib/jar/*.jar)|sed 's/, /:/g')"
