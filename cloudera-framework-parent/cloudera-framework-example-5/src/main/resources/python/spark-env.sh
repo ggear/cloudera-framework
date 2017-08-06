469e6946277bc35a3f5474fb171fb7041b0313fa
ggear <graham.gear@gmail.com>
2017-08-06 09:30:59 +0800

    Add NLTP resources to example-5

--- cloudera-framework-parent/cloudera-framework-example-5/src/main/resources/python/spark-env.sh
+++ cloudera-framework-parent/cloudera-framework-example-5/src/main/resources/python/spark-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-SPARK_CONFIG=cloudera-framework-parent/cloudera-framework-example-5/target/classes/spark/spark-defaults.xml
-PYSPARK_PYTHON=./NLTK/nltk_env/bin/python
+SPARK_CONFIG=cloudera-framework-parent/cloudera-framework-example-5/target/classes/python/spark-defaults.conf
+PYSPARK_PYTHON=./python/bin/python
--- cloudera-framework-parent/cloudera-framework-example-5/src/script/python/process.py
+++ cloudera-framework-parent/cloudera-framework-example-5/src/script/python/process.py
@@ -1,23 +1,36 @@
-import os
-import sys
+import nltk
 from pyspark.sql import SparkSession
 
+# @formatter:off
+!hdfs dfs -rm -f -r -skipTrash /tmp/stateunion
+!hdfs dfs -mkdir -p /tmp/stateunion/landing
+!hdfs dfs -put cloudera-framework-parent/cloudera-framework-example-5/src/test/resources/data/stateunion/nixon/1970/1970-Nixon.txt /tmp/stateunion/landing/1970-Nixon.txt
+!hdfs dfs -ls /tmp/stateunion/landing
+# @formatter:on
+
 spark = SparkSession.builder \
-         .appName("spark-nltk") \
-         .getOrCreate()
- 
-data = spark.sparkContext.textFile('/tmp/stateunion/1970-Nixon.txt')
- 
+    .appName("spark-nltk") \
+    .getOrCreate()
+
+data = spark.sparkContext.textFile('/tmp/stateunion/landing/1970-Nixon.txt')
+
+
 def word_tokenize(x):
-   import nltk
-   return nltk.word_tokenize(x)
- 
+    return nltk.word_tokenize(x)
+
+
 def pos_tag(x):
-   import nltk
-   return nltk.pos_tag([x])
- 
+    return nltk.pos_tag([x])
+
+
 words = data.flatMap(word_tokenize)
-words.saveAsTextFile('/tmp/stateunion/nixon_tokens')
- 
+words.saveAsTextFile('/tmp/stateunion/processed/tokens')
+
 pos_word = words.map(pos_tag)
-pos_word.saveAsTextFile('/tmp/stateunion/nixon_token_pos')
+pos_word.saveAsTextFile('/tmp/stateunion/processed/tokens_positions')
+
+# @formatter:off
+!hdfs dfs -ls /tmp/stateunion/processed
+!hdfs dfs -cat /tmp/stateunion/processed/tokens/*
+!hdfs dfs -cat /tmp/stateunion/processed/tokens_positions/*
+# @formatter:on
