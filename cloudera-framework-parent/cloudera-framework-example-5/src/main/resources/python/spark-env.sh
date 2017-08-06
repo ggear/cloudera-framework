2e71bdfe9c11cf743920b39a3e77b7e782d0dc9f
ggear <graham.gear@gmail.com>
2017-08-06 10:05:54 +0800

    Update example 5 spark paths

--- cloudera-framework-parent/cloudera-framework-example-5/src/main/resources/python/spark-env.sh
+++ cloudera-framework-parent/cloudera-framework-example-5/src/main/resources/python/spark-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SPARK_CONFIG=cloudera-framework-parent/cloudera-framework-example-5/target/classes/python/spark-defaults.conf
-PYSPARK_PYTHON=./python/bin/python
+PYSPARK_PYTHON=./cloudera-framework-example/python/bin/python
