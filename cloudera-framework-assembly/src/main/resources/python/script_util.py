ab3dc6466b617a5bf5a0bec2c122eca645c1d29f
ggear <graham.gear@gmail.com>
2017-11-05 07:01:13 +0800

    Update python script util to detect if paths are already fully qualified

--- cloudera-framework-assembly/src/main/resources/python/script_util.py
+++ cloudera-framework-assembly/src/main/resources/python/script_util.py
@@ -1,5 +1,7 @@
 import os
+import re
 
 
 def hdfs_make_qualified(path):
-    return path if 'CF_HADOOP_DEFAULT_FS' not in os.environ else os.environ['CF_HADOOP_DEFAULT_FS'] + path
+    return path if (re.match(r'[.]*://[.]*', path) or 'CF_HADOOP_DEFAULT_FS' not in os.environ) \
+        else os.environ['CF_HADOOP_DEFAULT_FS'] + path
