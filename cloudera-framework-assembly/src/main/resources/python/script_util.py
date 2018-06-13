5231d2ed12ed67083821dbc6ba208c6e3cd684f7
ggear <graham.gear@gmail.com>
2018-06-13 15:03:32 +0800

    Remove unecessary python code

--- cloudera-framework-assembly/src/main/resources/python/script_util.py
+++ cloudera-framework-assembly/src/main/resources/python/script_util.py
@@ -10,6 +10,6 @@ import re
 
 
 def qualify(path):
-    return path if (re.match(r'[.]*://[.]*', path) \
+    return path if (re.match(r'[.]*://[.]*', path)
                     or 'CF_HADOOP_DEFAULT_FS' not in os.environ) \
         else os.environ['CF_HADOOP_DEFAULT_FS'] + path
