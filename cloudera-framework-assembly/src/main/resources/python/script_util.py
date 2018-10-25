870aee7a3a414e4e7ea61fae82be0c8a79af6eb0
ggear <graham.gear@gmail.com>
2018-10-25 11:27:59 +0800

    Update python comments

--- cloudera-framework-assembly/src/main/resources/python/script_util.py
+++ cloudera-framework-assembly/src/main/resources/python/script_util.py
@@ -1,15 +1,16 @@
-###############################################################################
-#
-# Python script utilities as included from the cloudera-framework-assembly,
-# do not edit directly
-#
-###############################################################################
+"""
+Python script utilities as included from the cloudera-framework-assembly,
+do not edit directly
+"""
 
 import os
 import re
 
 
 def qualify(path):
+    """
+    Qualify an s3a:// or hdfs:// URL with environment defaults
+    """
     return path if (re.match(r'[.]*://[.]*', path)
                     or 'CF_HADOOP_DEFAULT_FS' not in os.environ) \
         else os.environ['CF_HADOOP_DEFAULT_FS'] + path
